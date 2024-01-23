418b7e38f9fc345c878f55135325be58e5b54f4b
justsicklife <justsicklife@gmail.com>
2024-01-23 23:21:05 +0900

    생성자 주입을 선택해라!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final  MemberRepository memberRepository;
-    private final  DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
